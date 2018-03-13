745b36f4c0e03ce7317926e33525d2ad0e378bf1
Twein <twein1989@gmail.com>
2018-03-13 17:26:43 +0800

    ...

--- test.go
+++ test.go
@@ -2,39 +2,31 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
-type SafeCounter struct {
-	v 	map[string] int
-	mux sync.Mutex
-}
-
-func (c *SafeCounter) Inc(key string) {
-	c.mux.Lock()
-	c.v[key]++
-	c.mux.Unlock()
-}
-
-func (c *SafeCounter) Value(key string) int {
-	c.mux.Lock()
-	defer c.mux.Unlock()
-	return c.v[key]
+func qsort(data []int) {
+	if len(data) <= 1 {
+		return 
+	}
+	mid, i := data[0], 1
+	head, tail := 0, len(data) - 1
+	for i = 1; i <= tail; {
+		if data[i] > mid {
+			data[i], data[tail] = data[tail], data[i]
+			tail--
+		} else {
+			data[i], data[head] = data[head], data[i]
+			head++
+			i++
+		}
+	}
+	data[head] = mid
+	qsort(data[:head])
+	qsort(data[head+1:])
 }
 
 func main() {
-	c := SafeCounter{v: make(map[string]int)}
-	for i := 0; i < 1000; i++ {
-		go c.Inc("somekey")
-	}
-	for i := 0; i < 500; i++ {
-		go c.Inc("222")
-	}
-
-	time.Sleep(time.Second)
-	fmt.Println(c.Value("somekey"))
-	fmt.Println(c.Value("222"))
-	fmt.Println(c)
+	a := []int{4,5,2,8,9,11}
+	qsort(a)
+	fmt.Println(a)
 }
-
